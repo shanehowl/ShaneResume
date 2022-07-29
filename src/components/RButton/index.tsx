d76d5be1bb85d588b5e73257ecd37dc3e2e9e0fe
Shane Ho <shaneho@Shanes-MacBook-Pro.local>
2022-07-29 11:43:47 +0800

    initial commit

---
+++ src/components/RButton/index.tsx
@@ -0,0 +1,10 @@
+import React, {type PropsWithChildren} from 'react';
+import {Text, View} from 'react-native';
+
+export const RButton: React.FC<PropsWithChildren<{}>> = ({}) => {
+  return (
+    <View>
+      <Text>Testing label</Text>
+    </View>
+  );
+};
